48142ba4053200dc828da6f32e2bd11fd185c165
futoke <futoke@gmail.com>
2023-06-29 13:50:50 +0300

    Upd

--- led.py
+++ led.py
@@ -1,11 +1,23 @@
+# from gpiozero import LED
+# from time import sleep
+
+
+# while True:
+#     for led_num in range(2, 27):
+#         led = LED(led_num)
+#         led.on()
+
+#         print('NYAAA')
+#         sleep(1)
+
+
 from gpiozero import LED
 from time import sleep
 
+red = LED(3)
 
 while True:
-    for led_num in range(2, 27):
-        led = LED(led_num)
-        led.on()
-
-        print('NYAAA')
-        sleep(1)
+    red.on()
+    sleep(1)
+    red.off()
+    sleep(1)
