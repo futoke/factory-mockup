9540477eb02a748757c7cecf1d7526665380a64b
futoke <futoke@gmail.com>
2023-07-06 07:45:11 +0300

    upd

--- ascon_server.py
+++ ascon_server.py
@@ -7,11 +7,11 @@ from socketserver import TCPServer
 from umodbus import conf
 from umodbus.server.tcp import RequestHandler, get_server
 
-# MODBUS_IP = '192.168.11.11'
-MODBUS_IP = '192.168.0.5'
+MODBUS_IP = '192.168.11.11'
+# MODBUS_IP = '192.168.0.5'
 MODBUS_PORT = 5020
-# PREFIX = '/home/ascon/factory-mockup'
-PREFIX = '/home/ichiro/factory-mockup'
+PREFIX = '/home/ascon/factory-mockup'
+# PREFIX = '/home/ichiro/factory-mockup'
 
 logging.basicConfig(
     handlers=[
